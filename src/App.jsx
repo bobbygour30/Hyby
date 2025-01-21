b36ec1d90ecbad821fd350d37a9cf8769a9b650c
bobby <bobby30gour@gamil.com>
2025-01-21 13:27:59 +0530

    Add loading screen and assets for improved user experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import assets from "./assets/assets";
+
+const LoadingScreen = () => {
+  return (
+    <div className="w-full flex flex-col justify-center items-center min-h-screen bg-gradient-to-b from-purple-100 via-white to-purple-100">
+      {/* Logo */}
+      <img
+        src={assets.logo}
+        alt="Logo"
+        className="w-52 mb-8 object-cover ml-2"
+      />
+
+      {/* Loading Animation */}
+      <div className="flex items-center justify-center space-x-2  w-full">
+        <span className="dot w-4 h-4 rounded-full bg-yellow-500 animate-bounce"></span>
+        <span
+          className="dot w-4 h-4 rounded-full bg-gray-500 animate-bounce"
+          style={{ animationDelay: "0.2s" }}
+        ></span>
+        <span
+          className="dot w-4 h-4 rounded-full bg-yellow-500 animate-bounce"
+          style={{ animationDelay: "0.4s" }}
+        ></span>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); 
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
+
   return (
-    
-      <div className="bg-gradient-to-b from-purple-100 via-white to-purple-100 min-h-screen no-scrollbar scroll-smooth flex flex-col items-center">
-        <div className="container mx-auto max-w-4xl">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-          <Footer />
+    <>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <div className="bg-gradient-to-b from-purple-100 via-white to-purple-100 min-h-screen no-scrollbar scroll-smooth flex flex-col items-center">
+          <div className="container mx-auto max-w-4xl">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+            <Footer />
+          </div>
         </div>
-      </div>
-    
+      )}
+    </>
   );
 };
 
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,8 @@ import influencer3 from './influencer3.jpg';
 import people1 from './people1.jpg';
 import people2 from './people2.jpg';
 import people3 from './people3.jpg';
+import loading from './loading.mp4';
+import logo from './logo.png';
 
 const assets = {
     influencer1,
@@ -12,6 +14,8 @@ const assets = {
     people1,
     people2,
     people3,
+    loading,
+    logo
 };
 
 export default assets;
